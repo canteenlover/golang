f9b91a3a6d8c9555a8a368fec13585707d27c8bc
sergey losev <75357413+canteenlover@users.noreply.github.com>
2023-04-22 01:50:16 +0300

    Add files via upload
    This code checks if your string is "right"
    Right means that strings starts with Capital letter and ends with dot "."

---
+++ CheckString.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"strings"
+	"unicode"
+)
+
+/*This code checks if your string is "right"
+* Right means that strings starts with Capital letter and ends with dot "."
+ */
+
+func main() {
+	var pref, suff bool
+	text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+
+	textRunes := []rune(text)
+	if unicode.IsUpper(textRunes[0]) {
+		pref = true
+	}
+
+	text = strings.TrimSuffix(text, "\r\n")
+
+	if strings.HasSuffix(text, ".") {
+		suff = true
+	}
+
+	if pref == true && suff == true {
+		fmt.Println("Right")
+	} else {
+		fmt.Println("Wrong")
+	}
+
+}
